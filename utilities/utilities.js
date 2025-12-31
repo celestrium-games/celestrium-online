caff9cb400c4faee080f59d1c762d09ed3f7b046
Krishav Anand <114196219+2026677@users.noreply.github.com>
2025-12-30 16:18:15 -0800

    Fix syntax error in utilities.js

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -1,3 +1,4 @@
 const utilities = [
-    {name:"About:Blank Tab Cloaker", url:"./utilities/about-blank-cloaker/"}
+    {name:"About:Blank Tab Cloaker", url:"./utilities/about-blank-cloaker/"},
+    {name:"Ruffle SWF Player", url:"./utilities/ruffle/"}
 ];
